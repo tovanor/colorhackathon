48b4063f824231818adbdf23d5018111751453c5
Kat Nguyen <kat@Kat-Nguyens-MacBook-Pro.local>
2011-07-16 00:36:24 -0700

    added font styles

--- inc/meta.inc.php
+++ inc/meta.inc.php
@@ -4,3 +4,4 @@
 <link rel="stylesheet" href="<?php echo CPATH ?>style/text.css" />
 <link rel="stylesheet" href="<?php echo CPATH ?>style/960.css" />
 <link rel="stylesheet" href="<?php echo CPATH ?>style/screen.css" />
+<link href='http://fonts.googleapis.com/css?family=Indie+Flower|Varela+Round&v2' rel='stylesheet' type='text/css'>
