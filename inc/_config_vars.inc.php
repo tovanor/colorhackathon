56a557dbc1d0163eb1fa3842024bab17416e34ca
Ming Han Teh <teh_minghan@hotmail.com>
2011-07-15 21:32:18 -0700

    Added config.inc.php

---
+++ inc/_config_vars.inc.php
@@ -0,0 +1,15 @@
+<?php
+
+// rename this as config.inc.php
+
+global $_c;
+
+$_c["mysql_host"] = "127.0.0.1"; 	// MySQL hostname or IP address 
+$_c["mysql_user"] = ""; 			// MySQL user
+$_c["mysql_pass"] = "";				// MySQL password
+$_c["mysql_db"] = "color";			// MySQL database name
+
+
+define("CPATH", "/~minghan/color/"); // absolute path of URL
+
+?>
