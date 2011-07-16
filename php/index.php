e720f42bf251cbf363835607ad27d6e94bae7a1c
Leslie Apland <quicque@gmail.com>
2011-07-15 20:34:03 -0700

    Put the php files in a php/ folder and removed color dump (we have colordump.sql).

--- php/index.php
+++ php/index.php
@@ -1,12 +1,14 @@
 <?php
 
 // display header
-
+require_once("header.inc.php");
 // logged in => "create new thread"
 // not logged in => "log in"
 
+<p>
+Welcome to the index page!
+</p>
 
-
-// dispaly footer
-
+// display footer
+require_once("footer.inc.php");
 ?>
