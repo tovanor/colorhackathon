fb135f7cf865cae98861b87e58d1750bc704400a
Leslie Apland <quicque@gmail.com>
2011-07-16 06:03:18 -0700

    Added checks to make the index not crash because of the facebook code.

--- index.php
+++ index.php
@@ -2,41 +2,40 @@
 $title = "Index!";
 require_once("inc/header.inc.php");
 
-require 'php-sdk/src/facebook.php';
+if($_SERVER['DOCUMENT_ROOT'] != "/var/www") {
+	require 'php-sdk/src/facebook.php';
 
-// Create our Application instance (replace this with your appId and secret).
-$facebook = new Facebook($_c['fb']);
+	// Create our Application instance (replace this with your appId and secret).
+	$facebook = new Facebook($_c['fb']);
 
 
 
-// Get User ID
-$user = $facebook->getUser();
+	// Get User ID
+	$user = $facebook->getUser();
 
-// We may or may not have this data based on whether the user is logged in.
-//
-// If we have a $user id here, it means we know the user is logged into
-// Facebook, but we don't know if the access token is valid. An access
-// token is invalid if the user logged out of Facebook.
+	// We may or may not have this data based on whether the user is logged in.
+	//
+	// If we have a $user id here, it means we know the user is logged into
+	// Facebook, but we don't know if the access token is valid. An access
+	// token is invalid if the user logged out of Facebook.
 
-if ($user) {
-  try {
-    // Proceed knowing you have a logged in user who's authenticated.
-    $user_profile = $facebook->api('/me');
-  } catch (FacebookApiException $e) {
-    error_log($e);
-    $user = null;
-  }
+	if ($user) {
+	  try {
+		// Proceed knowing you have a logged in user who's authenticated.
+		$user_profile = $facebook->api('/me');
+	  } catch (FacebookApiException $e) {
+		error_log($e);
+		$user = null;
+	  }
+	}
+
+	// Login or logout url will be needed depending on current user state.
+	if ($user) {
+	  $logoutUrl = $facebook->getLogoutUrl();
+	} else {
+	  $loginUrl = $facebook->getLoginUrl();
+	}
 }
-
-// Login or logout url will be needed depending on current user state.
-if ($user) {
-  $logoutUrl = $facebook->getLogoutUrl();
-} else {
-  $loginUrl = $facebook->getLoginUrl();
-}
-
-
-
 
  ?>
 
@@ -61,30 +60,26 @@ while($t = $completed_threads->fetch_object()) {
 	echo '<a href="showthread.php?id=' . $t->id . '">' . $ft->content . '</a><br />';
 }
 
-?>
+if($_SERVER['DOCUMENT_ROOT'] != "/var/www") {
+	?>
 
-    <h1>php-sdk</h1>
+		<h1>php-sdk</h1>
 
-    <?php if ($user): ?>
-      <a href="<?php echo $logoutUrl; ?>">Logout</a>
-    <?php else: ?>
-      <div>
-        Login using OAuth 2.0 handled by the PHP SDK:
-        <a href="<?php echo $loginUrl; ?>">Login with Facebook</a>
-      </div>
-    <?php endif ?>
+		<?php if ($user): ?>
+		  <a href="<?php echo $logoutUrl; ?>">Logout</a>
+		<?php else: ?>
+		  <div>
+		    Login using OAuth 2.0 handled by the PHP SDK:
+		    <a href="<?php echo $loginUrl; ?>">Login with Facebook</a>
+		  </div>
+		<?php endif ?>
 
-    <?php if ($user): ?>
-      <h3>You</h3>
-      <img src="https://graph.facebook.com/<?php echo $user; ?>/picture">
-         <?php else: ?>
-      <strong><em>You are not Connected.</em></strong>
-    <?php endif ?>
-
-<?php require_once("inc/footer.inc.php"); ?>
-
-
-
- 
-
- 
+		<?php if ($user): ?>
+		  <h3>You</h3>
+		  <img src="https://graph.facebook.com/<?php echo $user; ?>/picture">
+		     <?php else: ?>
+		  <strong><em>You are not Connected.</em></strong>
+		<?php endif ?>
+<?php
+}
+require_once("inc/footer.inc.php"); ?>
