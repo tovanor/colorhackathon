1e52e0c34c731f3d8ca5e451ec386b2e9f719df4
stanleynwang <stanley@ning.com>
2011-07-15 20:38:17 -0700

    Tweaked index.php

--- php/index.php
+++ php/index.php
@@ -4,11 +4,13 @@
 require_once("header.inc.php");
 // logged in => "create new thread"
 // not logged in => "log in"
+?>
 
 <p>
 Welcome to the index page!
 </p>
 
+<?php
 // display footer
 require_once("footer.inc.php");
 ?>
