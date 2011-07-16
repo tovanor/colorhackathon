a31591427c9c2e486387308ef8c237dccc74b5f5
Ming Han Teh <teh_minghan@hotmail.com>
2011-07-15 22:06:45 -0700

    Index

--- index.php
+++ index.php
@@ -1,23 +1,5 @@
-<? require_once("inc/config.inc.php") ?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<?php require_once("inc/header.inc.php"); ?>
 
-<head>
-	<title>Pictionary</title>
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" /> 
-	<?php require_once("inc/meta.inc.php") ?>
-</head>
+<h1>Hello World</h1>
 
-<body>
-
-<?php require_once("include/header.inc.php") ?>
-
-<div class="main container_12">
-
-
-</div>
-
-<?php require_once("include/header.inc.php") ?>
-
-</body>
-
-</html>
+<?php require_once("inc/header.inc.php"); ?>
