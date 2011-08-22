3f41de57229f845d30b65095fb05694f9ea7db81
Adrian Lee <adrian@ning.com>
2011-08-22 14:18:20 -0700

    Minor changes. Change thread length to 8 and page title to comicrazy.

--- inc/header.inc.php
+++ inc/header.inc.php
@@ -1,14 +1,14 @@
-<?php 
-require_once("config.inc.php"); ?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<?php require_once("config.inc.php"); ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <html xmlns="http://www.w3.org/1999/xhtml" xmlns:fb="https://www.facebook.com/2008/fbml">
 
 
 <head>
-	<title>Pictionary - <?php echo $title; ?></title>
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" /> 
-	<link rel="icon" href="images/favicon.ico" type="image/x-icon"> 
-	<?php require_once("meta.inc.php") ?>
+    <title>Comicrazy - <?php echo $title; ?></title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <link rel="icon" href="images/favicon.ico" type="image/x-icon">
+    <?php require_once("meta.inc.php") ?>
 </head>
 
 <body>
